0a94385b18feb47509e7dd30d7546622d9435802
Alexandra Camarena <alexandracamarena@ou.edu>
2024-02-12 23:46:26 -0600

    Add project files.

---
+++ LCP0214/Program.cs
@@ -0,0 +1,22 @@
+﻿// MIS 3013 001
+// Feb 14, 2024
+// Alexandra Camarena
+// 113568154
+
+Console.WriteLine("% ");
+// between 10 and 200, 3 & 5
+// 10 15 
+
+int N;
+N = 0;
+
+for (int i = 10; i <= 200; i = i + 1)
+{
+    if (i % 3 ==0 && i%5==0)
+    {
+        N = N + 1;
+        Console.WriteLine(i);
+    }
+}
+
+Console.WriteLine(N);
